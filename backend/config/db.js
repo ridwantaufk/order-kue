f5f50d3e601f96b73090847dcf4a30f02b3bf4a7
ridwantaufk <ridwan1998taufik@gmail.com>
2024-12-03 06:59:05 +0700

    21 - mengganti konfigurasi database

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,35 +3,35 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 // SET DATABASE PUBLIC
-const dbUrl = process.env.DB_URL;
-const { username, password, hostname, port, pathname } = new URL(dbUrl);
+// const dbUrl = process.env.DB_URL;
+// const { username, password, hostname, port, pathname } = new URL(dbUrl);
 
-// Menghapus leading '/' pada pathname untuk mendapatkan nama database
-const database = pathname.replace(/^\//, "");
+// // Menghapus leading '/' pada pathname untuk mendapatkan nama database
+// const database = pathname.replace(/^\//, "");
 
-console.log(
-  `Username: ${username}, Password: ${password}, Host: ${hostname}, Port: ${port}, Database: ${database}`
-);
+// console.log(
+//   `Username: ${username}, Password: ${password}, Host: ${hostname}, Port: ${port}, Database: ${database}`
+// );
 
-const sequelize = new Sequelize(database, username, password, {
-  host: hostname, // gunakan hostname langsung
-  dialect: process.env.DB_DIALECT, // pastikan ini diset di .env
-  port: Number(port), // Port harus berupa angka
-  timezone: process.env.DB_TIMEZONE || "+07:00", // Default timezone jika tidak ada
-  logging: console.log, // Opsional
-});
+// const sequelize = new Sequelize(database, username, password, {
+//   host: hostname, // gunakan hostname langsung
+//   dialect: process.env.DB_DIALECT, // pastikan ini diset di .env
+//   port: Number(port), // Port harus berupa angka
+//   timezone: process.env.DB_TIMEZONE || "+07:00", // Default timezone jika tidak ada
+//   logging: console.log, // Opsional
+// });
 
 // SET DATABASE LOCAL
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: process.env.DB_DIALECT,
-//     timezone: process.env.DB_TIMEZONE,
-//   }
-// );
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    timezone: process.env.DB_TIMEZONE,
+  }
+);
 
 sequelize
   .authenticate()
