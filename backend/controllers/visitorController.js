67f96df82881b6b9979716276faf709694e57a3a
ridwantaufk <ridwan1998taufik@gmail.com>
2024-12-12 05:01:06 +0700

    38 - penambahan latitude dan longitude lokasi pengunjung

--- backend/controllers/visitorController.js
+++ backend/controllers/visitorController.js
@@ -4,17 +4,23 @@ const geoip = require("geoip-lite");
 const recordVisitor = async (req, res) => {
   try {
     const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+
     const location = geoip.lookup(ip) || {
       country: "Tidak Diketahui",
       region: "Tidak Diketahui",
       city: "Tidak Diketahui",
+      ll: [0, 0], // default latitude dan longitude
     };
+
+    console.log("location : ", location);
 
     const visitorInfo = {
       ip_address: ip,
       country: location.country,
       region: location.region,
       city: location.city,
+      latitude: location.ll[0],
+      longitude: location.ll[1],
       page_visited: req.body.page,
       visit_time: new Date(),
       user_agent: req.headers["user-agent"],
